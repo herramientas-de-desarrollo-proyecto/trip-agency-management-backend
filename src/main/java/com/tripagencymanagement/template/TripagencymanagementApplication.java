c9a10998ad0d75bdebd9ebf07608e883967979e1
Gonzalo Gutiérrez Castillo <gonzalogut.99@gmail.com>
2026-01-18 18:25:39 -0500

    first commit

---
+++ src/main/java/com/tripagencymanagement/template/TripagencymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.tripagencymanagement.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripagencymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripagencymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripagencymanagement/template/TripagencymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripagencymanagement.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripagencymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
